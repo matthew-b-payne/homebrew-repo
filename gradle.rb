4c81149daba2ea173aaf95cae8e7fc8e2196bf9b
Matthew Payne <matthew.b.payne@gmail.com>
2020-08-03 16:00:12 -0400

    Update gradle.rb

--- gradle.rb
+++ gradle.rb
@@ -2,7 +2,7 @@ class Gradle < Formula
   homepage "https://www.gradle.org/"
    sha256 'https://services.gradle.org/distributions/gradle-4.10.3-bin.zip.sha256'
 
-   url "https://services.gradle.org/distributions/gradle-4.10.3-all.zip"
+   url "https://services.gradle.org/distributions/gradle-4.10.3-bin.zip"
 
   def install
     libexec.install %w[bin lib]
