86d6db9d470bc9d1a3108e3f9602d509adabf8da
Matthew Payne <matthew.b.payne@gmail.com>
2020-08-03 16:01:06 -0400

    Update gradle.rb

--- gradle.rb
+++ gradle.rb
@@ -1,6 +1,6 @@
 class Gradle < Formula
   homepage "https://www.gradle.org/"
-   sha256 'https://services.gradle.org/distributions/gradle-4.10.3-bin.zip.sha256'
+   #sha256 'https://services.gradle.org/distributions/gradle-4.10.3-bin.zip.sha256'
 
    url "https://services.gradle.org/distributions/gradle-4.10.3-bin.zip"
 
